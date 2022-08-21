23e5a25ca1fbea3fd5d270ef0554b18712094d86
Matheus Alves <matheusalves.bmd@gmail.com>
2022-08-20 23:04:26 -0300

    Set up webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const { mode } = require("webpack-nano/argv");
-const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
-const { WebpackPluginServe } = require("webpack-plugin-serve");
+const { merge } = require("webpack-merge");
+const parts = require("./webpack.parts");
 
-module.exports = {
-  /* Is mode equals in value and type to development? */
-  watch: mode === "development",
-  entry: ["./src", "webpack-plugin-serve/client"],
-  mode,
-  plugins: [
-    new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
-    new WebpackPluginServe({
-      port: parseInt(process.env.PORT, 10) || 8080,
-      static: "./dist",
-      liveReload: true,
-      waitForBuild: true,
-    }),
-  ],
+const commonConfig = merge([{ entry: ["./src"] }, parts.page({ title: "Demo" })]);
+const productionConfig = merge([]);
+const developmentConfig = merge([{ entry: "webpack-plugin-serve/client" }, parts.devServer()]);
+
+const getConfig = mode => {
+  switch (mode) {
+    case "production":
+      return merge(commonConfig, productionConfig, { mode });
+    case "development":
+      return merge(commonConfig, developmentConfig, { mode });
+    default:
+      throw new Error(`Trying to use an unknown mode, ${mode}`);
+  }
 };
+
+module.exports = getConfig(mode);
