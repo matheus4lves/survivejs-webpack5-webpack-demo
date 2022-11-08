fcc251ff09fc390750c68c5b71d121f9a7d1064a
Matheus Alves <matheusalves.bmd@gmail.com>
2022-11-07 23:26:28 -0300

    Demonstrate the tree shaking feature
    If you run the build script and search for "bake" in the main.js code,
    you'll find it. But if you look for "shake", you'll not be able to find
    it since it has been dropped from the code once it is not used.

---
+++ src/components/shake.js
@@ -0,0 +1,5 @@
+// Remember that Named Exports are what we have here
+const shake = () => console.log("shake");
+const bake = () => console.log("bake");
+
+export { shake, bake };
--- src/index.js
+++ src/index.js
@@ -12,5 +12,10 @@ import component from "./components/component";
 import paragraph from "./components/paragraph";
 import "./main.css";
 
+// Remember that here you have an import from a named export
+import { bake } from "./components/shake";
+
+bake();
+
 document.body.appendChild(component());
 document.body.appendChild(paragraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tristique lorem suscipit, congue tortor eu, porttitor libero. Integer tempus commodo justo eu gravida."));
