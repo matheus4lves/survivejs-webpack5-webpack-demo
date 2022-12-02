6e6accf3c9f5140907a14467eddeba91d94a4f80
Matheus Alves <matheusalves.bmd@gmail.com>
2022-12-02 19:55:05 -0300

    Use records
    Records are used for storing module IDs across separate builds.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const mode = process.env.mode;
 const { merge } = require("webpack-merge");
 const parts = require("./webpack.parts");
 const cssLoaders = [parts.tailwind()];
+const path = require("path");
 
 const commonConfig = merge([{ output: { chunkFilename: "[name].[contenthash].js", filename: "[name].[contenthash].js", assetModuleFilename: "[name].[contenthash].[ext].[query]" } }, { entry: ["./src"] }, parts.extractCss({ loaders: cssLoaders }), parts.page({ title: "Demo" }), parts.loadImages({ limit: 8192 }), parts.clean(), parts.readEnvironmentVariables()]);
-const productionConfig = merge([parts.minifyCSS({ options: { preset: ["default"] } }), parts.minifyJavaScript(), parts.eliminateUnusedCss(), parts.loadJavaScript(), parts.generateSourceMaps({ type: "source-map" }), { optimization: { splitChunks: { chunks: "all" }, runtimeChunk: { name: "runtime" } } }]);
+const productionConfig = merge([parts.minifyCSS({ options: { preset: ["default"] } }), parts.minifyJavaScript(), parts.eliminateUnusedCss(), parts.loadJavaScript(), parts.generateSourceMaps({ type: "source-map" }), { optimization: { splitChunks: { chunks: "all" }, runtimeChunk: { name: "runtime" } } }, { recordsPath: path.join(__dirname, "records.json") }]);
 const developmentConfig = merge([parts.devServer()]);
 
 const getConfig = mode => {
