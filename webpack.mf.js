fc11a214d990f8d2b9a1014bc2c3876d33c4cd6e
Matheus Alves <matheusalves.bmd@gmail.com>
2023-03-29 19:02:04 -0300

    Implement the Module Federation example

---
+++ webpack.mf.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const { component, mode } = require("webpack-nano/argv");
+const { merge } = require("webpack-merge");
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+  {
+    output: { publicPath: "/" },
+  },
+  parts.loadJavaScript(),
+  parts.loadImages(),
+  parts.page(),
+  parts.extractCss({ loaders: [parts.tailwind()] }),
+]);
+
+const configs = {
+  development: merge({ entry: ["webpack-plugin-serve/client"] }, parts.devServer()),
+  production: {},
+};
+
+const shared = {
+  react: { singleton: true },
+  "react-dom": { singleton: true },
+};
+const componentConfigs = {
+  app: merge(
+    {
+      entry: [path.join(__dirname, "src", "bootstrap.js")],
+    },
+    parts.page(),
+    parts.federateModule({
+      name: "app",
+      remotes: { mf: "mf@/mf.js" },
+      shared,
+    })
+  ),
+  header: merge(
+    {
+      entry: [path.join(__dirname, "src", "header.js")],
+    },
+    parts.federateModule({
+      name: "mf",
+      filename: "mf.js",
+      exposes: { "./header": "./src/header" },
+      shared,
+    })
+  ),
+};
+
+if (!component) throw new Error("Missing component name");
+
+module.exports = merge(commonConfig, configs[mode], { mode }, componentConfigs[component]);
--- webpack.parts.js
+++ webpack.parts.js
@@ -7,6 +7,7 @@ const PurgeCssPlugin = require("purgecss-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 const ALL_FILES = glob.sync(path.join(__dirname, "src/*.js"));
 
@@ -21,6 +22,19 @@ exports.devServer = () => ({
   },
 });
 
+/* {title = title, url = "", chunks = chunks} = {}
+First, the destructured left-hand side has a default value of an empt obj:
+obj = {}. If an object is not provided, this empty object will be used.
+Second, we provide some default values for the destructured obj. If a property
+is no present in the obj passed as an argument to the function, those values will
+be used instead. Note that if no object is passed at all, the empty obj will be 
+used as the default value for the destructuing assigment and, since the empty obj
+has no property of its own, all default values in the left-hand side of the 
+destructuring assigment will be used.
+
+See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#syntax
+Look at the "Setting a function parameter's default value" example
+*/
 exports.page = ({ title, url = "", chunks } = {}) => ({
   plugins: [
     new MiniHtmlWebpackPlugin({
@@ -134,4 +148,16 @@ exports.minifyCSS = ({ options }) => ({
 exports.readEnvironmentVariables = () => ({
   // Prefix was set so you don't have to type "process.env."
   plugins: [new Dotenv({ prefix: "" })],
+});
+
+exports.federateModule = ({ name, filename, exposes, remotes, shared }) => ({
+  plugins: [
+    new ModuleFederationPlugin({
+      name,
+      filename,
+      exposes,
+      remotes,
+      shared,
+    }),
+  ],
 });
